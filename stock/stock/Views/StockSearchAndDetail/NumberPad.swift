0865eac39a334f1ce5b96c4c6d023c649cb1f465
Po-Chu Chen <po@Po-ChudeMacBook-Pro.local>
2024-05-24 21:39:22 -0700

    add show type of transaction card: buy-order and sell-order

---
+++ stock/stock/Views/StockSearchAndDetail/NumberPad.swift
@@ -0,0 +1,18 @@
+//
+//  NumberPad.swift
+//  stock
+//
+//  Created by Po-Chu Chen on 5/23/24.
+//
+
+import SwiftUI
+
+struct NumberPad: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumberPad()
+}
---
+++ stock/stock/Views/StockSearchAndDetail/SellPlaceOrderView.swift
@@ -0,0 +1,18 @@
+//
+//  SellPlaceOrderView.swift
+//  stock
+//
+//  Created by Po-Chu Chen on 5/23/24.
+//
+
+import SwiftUI
+
+struct SellPlaceOrderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SellPlaceOrderView()
+}
