8d5eeb6a7b1269a5882128d736d66b4302f0e1d7
pcchencode <zxp791228@gmail.com>
2024-05-31 00:30:21 -0700

    refract the code

--- stock/stock/Views/MainPageView.swift
+++ stock/stock/Views/MainPageView.swift
@@ -25,76 +25,90 @@ struct MainPageView: View {
     @State private var isShowingStockSearchSheet: Bool = false
     @State private var isShowingEditWatchlistSheet: Bool = false
     @State private var refreshID = UUID()
+    @State private var isRefreshing: Bool = false
     
     var body: some View {
         NavigationView {
-            ZStack {
-                VStack {
-                    Spacer()
-                        .frame(height: 130) // 頂部的留白區域，確保 `HeaderView` 不會遮擋 `RefreshableScrollView`
-                    
-                    RefreshableScrollView {
-                        VStack(spacing: 16) { // 添加適當的間距
-                            PortfolioCardView()
-                                .padding(.top, 0) // 移除 PortfolioCardView 的頂部間距
-                            
-                            PortfolioView()
-                            
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack {
-                                    ForEach(portfolioItems, id: \.self) { portfolioItem in
-                                        NavigationLink(destination: StockDetailView(stockName: portfolioItem.stockName ?? "", stockId: portfolioItem.stockId ?? "", stockExchange: portfolioItem.stockExchange ?? "")) {
-                                            PortfolioCards(
-                                                stockId: portfolioItem.stockId ?? "",
-                                                stockName: portfolioItem.stockName ?? "",
-                                                stockExchange: portfolioItem.stockExchange ?? "",
-                                                stockQuantity: portfolioItem.quantity
-                                            )
-                                        }
-                                        .buttonStyle(PlainButtonStyle())
-                                    }
-                                }
-                            }
-                            
-                            WatchListView(showSheet: $isShowingEditWatchlistSheet)
-
-                            ScrollView {
-                                ForEach(items, id: \.self) { item in
-                                    NavigationLink(destination: StockDetailView(stockName: item.stockName ?? "", stockId: item.stockId ?? "", stockExchange: item.stockExchange ?? "")) {
-                                        StockCardView(stockName: item.stockName ?? "", stockId: item.stockId ?? "", stockExchange: item.stockExchange ?? "")
+            VStack(spacing: 0) {
+                // 使用 `GeometryReader` 监控 `ScrollView` 的滚动偏移量
+                RefreshableScrollView {
+                    VStack() {
+                        //Color.white.frame(height: 5)
+                        InfiniteScrollView()
+                            .frame(height: 60)
+                        
+                        PortfolioCardView()
+                            //.padding(.top, 0) // 移除 PortfolioCardView 的顶部间距
+                        
+                        PortfolioView()
+                        
+                        ScrollView(.horizontal, showsIndicators: false) {
+                            HStack {
+                                ForEach(portfolioItems, id: \.self) { portfolioItem in
+                                    NavigationLink(destination: StockDetailView(stockName: portfolioItem.stockName ?? "", stockId: portfolioItem.stockId ?? "", stockExchange: portfolioItem.stockExchange ?? "")) {
+                                        PortfolioCards(
+                                            stockId: portfolioItem.stockId ?? "",
+                                            stockName: portfolioItem.stockName ?? "",
+                                            stockExchange: portfolioItem.stockExchange ?? "",
+                                            stockQuantity: portfolioItem.quantity
+                                        )
                                     }
                                     .buttonStyle(PlainButtonStyle())
                                 }
                             }
-                            .frame(maxWidth: .infinity, maxHeight: 500)
-                            
-                            Spacer()
                         }
-                        .padding()
-                        .edgesIgnoringSafeArea(.bottom)
-                        .sheet(isPresented: $isShowingStockSearchSheet) {
-                            StockSearchView()
+                        
+                        WatchListView(showSheet: $isShowingEditWatchlistSheet)
+                        
+                        ScrollView {
+                            ForEach(items, id: \.self) { item in
+                                NavigationLink(destination: StockDetailView(stockName: item.stockName ?? "", stockId: item.stockId ?? "", stockExchange: item.stockExchange ?? "")) {
+                                    StockCardView(stockName: item.stockName ?? "", stockId: item.stockId ?? "", stockExchange: item.stockExchange ?? "")
+                                }
+                                .buttonStyle(PlainButtonStyle())
+                            }
                         }
-                        .sheet(isPresented: $isShowingEditWatchlistSheet) {
-                            EditWatchlistView()
-                        }
-                    } onRefresh: {
-                        DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
-                            refreshID = UUID() // 改变 ID 以触发视图重新加载
-                        }
+                        .frame(maxWidth: .infinity, maxHeight: 500)
+                        
+                        Spacer()
                     }
-                    .id(refreshID)
+                    .padding()
+                    .edgesIgnoringSafeArea(.bottom)
+                    .sheet(isPresented: $isShowingStockSearchSheet) {
+                        StockSearchView()
+                    }
+                    .sheet(isPresented: $isShowingEditWatchlistSheet) {
+                        EditWatchlistView()
+                    }
+                } onRefresh: {
+                    withAnimation {
+                        isRefreshing = true
+                    }
+                    DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                        withAnimation {
+                            isRefreshing = false
+                        }
+                        refreshID = UUID() // 改变 ID 以触发视图重新加载
+                    }
                 }
-                
-                VStack {
+                .id(refreshID)
+            }
+            .navigationTitle("Dashboard")
+            .navigationBarTitleDisplayMode(.inline)
+            .edgesIgnoringSafeArea(.top)
+            // New modifiers
+            .toolbarBackground(.clear, for: .navigationBar)
+            //.toolbarColorScheme(.dark, for: .navigationBar)
+            //.toolbarBackground(.visible, for: .navigationBar)
+
+            .toolbar {
+                ToolbarItem(placement: .principal) {
                     HeaderView(showSheet: $isShowingStockSearchSheet)
-                        .background(.ultraThinMaterial) // 使用毛玻璃效果背景
-                        .padding(.top, 0) // 調整頂部padding
-                        .zIndex(1)
-                    Spacer()
+                        //.background(.ultraThinMaterial) // 使用毛玻璃效果背景
                 }
             }
         }
+
     }
 }
 
