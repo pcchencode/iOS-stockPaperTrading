0a448443191eb8328cee358dbab130d40944bb95
pcchencode <zxp791228@gmail.com>
2024-05-31 00:27:48 -0700

    update

---
+++ stock/stock/InfiniteScrollView.swift
@@ -0,0 +1,18 @@
+//
+//  InfiniteScrollView.swift
+//  stock
+//
+//  Created by Po-Chu Chen on 5/31/24.
+//
+
+import SwiftUI
+
+struct InfiniteScrollView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InfiniteScrollView()
+}
